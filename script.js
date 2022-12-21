577ccf05451cc53733bc1a92251da8163a3e3694
valek-web <ziryanovvalentin@mail.ru>
2022-12-21 21:04:53 +0300

    menu_d

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const open_btn = document.querySelector('.open')
 const menu = document.querySelector('.menu')
 const body = document.querySelector('body')
 const bgFor_aM = document.querySelector('.bgFor_aM')
+const menu_elements = document.querySelectorAll('.menu__element')
 
 open_btn.addEventListener('click', () => {
     menu.classList.add('menu_isActive')
@@ -15,3 +16,11 @@ close_btn.addEventListener('click', () => {
     body.style.overflow = "auto"
     bgFor_aM.classList.toggle('bgFor_aM_active') 
 })
+
+for (let i = 0; i < menu_elements.length; i++) {
+    menu_elements[i].addEventListener('click', () => {
+        menu.classList.toggle('menu_isActive')
+        body.style.overflow = "auto"
+        bgFor_aM.classList.toggle('bgFor_aM_active') 
+    })
+  }
